af9bf27c101b938ee1713e67babd896e6835ab69
Ad <chuck.rv.ua@gmail.com>
2016-09-22 20:16:37 +0300

    sss12

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,62 +1,10 @@
-ContactManager.module('ContactsApp.List', function (List, ContactManager,Backbone, Marionette, $, _){
+ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Contact = Marionette.ItemView.extend({
-    tagName: "tr",
-    template: "#contact-list-item",
-
-    events: {
-      "click": "highlightName",
-      "click td a.js-show": "showClicked",
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      "click button.js-delete": "deleteClicked"
-    },
-    showClicked: function(e){
-      e.preventDefault();
-      e.stopPropagation();
-      this.trigger("contact:show", this.model);
-    },
-
-    remove: function(){
-      var self=this;
-      this.$el.fadeOut(function(){
-        Marionette.ItemView.prototype.remove.call(self);
-      });
-    },
-
-    highlightName: function (e) {
-      this.$el.toggleClass("warning");
-      this.trigger("contact:highlighting:toggled", this.model);
-    },
-    deleteClicked: function(e){
-      e.stopPropagation();
-      this.trigger("contact:delete",this.model);
-    }
+    tagName: "li",
+    template: "#contact-list-item"
   });
-
-  List.Contacts = Marionette.CompositeView.extend({
-    tagName: "table",
-    className: "table table-hover",
-    template: "#contact-list",
-    childView: List.Contact,
-    childViewContainer: "tbody"
-      });
+  List.Contacts = Marionette.CollectionView.extend({
+    tagName: "ul",
+    childView: List.Contact
+  });
 });
